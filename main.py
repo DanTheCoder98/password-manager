2d836fa42bfbbd31e153d5e091ad0ac1500cefa6
Daniel Sarney <daniel_sarney@live.co.uk>
2024-11-01 18:40:20 +0000

    Password Generator Func

--- main.py
+++ main.py
@@ -1,21 +1,114 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_input.delete(0, END)
+    password_input.insert(0, password)
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     with open("data.txt", "a") as data_file:
-        data_file.write(
-            f"{website_input.get()} | {email_input.get()} | {password_input.get()}\n"
+        website = website_input.get()
+        email = email_input.get()
+        password = password_input.get()
+
+        if len(website) == 0 or len(password) == 0 or len(email) == 0:
+            messagebox.showinfo(
+                title="Oops",
+                message="Please make sure you haven't left any fields empty.",
+            )
+            return
+
+        is_ok = messagebox.askyesnocancel(
+            title=website,
+            message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?",
         )
+
+        if not is_ok:
+            return
+
+        data_file.write(f"{website} | {email} | {password}\n")
 
     website_input.delete(0, END)
     email_input.delete(0, END)
     password_input.delete(0, END)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -44,7 +137,7 @@ password_label.grid(column=0, row=3)
 password_input = Entry(width=22)
 password_input.grid(column=1, row=3)
 
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
 save_button = Button(text="Save", width=37, command=save)
