e0255ccfcfee59cb25afb9f6175689e9868b8f58
Daniel Sarney <daniel_sarney@live.co.uk>
2024-11-01 17:35:25 +0000

    Initial Upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=1, padx=20, pady=20)
+
+window.mainloop()
