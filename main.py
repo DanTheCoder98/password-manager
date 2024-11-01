6e5611155883ca5d4e46647809b5ba04702486ec
Daniel Sarney <daniel_sarney@live.co.uk>
2024-11-01 17:50:18 +0000

    UI Setup

--- main.py
+++ main.py
@@ -9,9 +9,33 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=200, height=200)
+canvas = Canvas(width=300, height=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=1)
+canvas.create_image(200, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_input = Entry(width=40)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_imput = Entry(width=40)
+email_imput.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_input = Entry(width=20)
+password_input.grid(column=1, row=3)
+
+generate_password_button = Button(text="Generate Password", width=16)
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=40)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
